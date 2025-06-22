baefebd768199da6e46847648c3c868c197f329b
mariflor <mariflor.vega@gmail.com>
2025-06-23 00:42:57 +0100

    Initial commit

--- src/model/train.py
+++ src/model/train.py
@@ -3,42 +3,48 @@
 import argparse
 import glob
 import os
-
+import mlflow
 import pandas as pd
 
 from sklearn.linear_model import LogisticRegression
-
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+import joblib
+from sklearn.datasets import load_diabetes
 
 # define functions
 def main(args):
     # TO DO: enable autologging
+    #mlflow.autolog()
 
 
     # read data
-    df = get_csvs_df(args.training_data)
+    df = pd.read_csv(args.training_data)
 
     # split data
     X_train, X_test, y_train, y_test = split_data(df)
 
     # train model
-    train_model(args.reg_rate, X_train, X_test, y_train, y_test)
+    model = train_model(args.reg_rate, X_train, y_train)
+
+    preds = model.predict(X_test)
+    mse = mean_squared_error(preds, y_test)
+    metrics = {"mse": mse}
+    print(metrics)
+
+    model_name = "sklearn_regression_model.pkl"
+    joblib.dump(value=model, filename=model_name)
+
+def split_data(df):
+    X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+    return X_train, X_test, y_train, y_test 
 
 
-def get_csvs_df(path):
-    if not os.path.exists(path):
-        raise RuntimeError(f"Cannot use non-existent path provided: {path}")
-    csv_files = glob.glob(f"{path}/*.csv")
-    if not csv_files:
-        raise RuntimeError(f"No CSV files found in provided data path: {path}")
-    return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
-
-
-# TO DO: add function to split data
-
-
-def train_model(reg_rate, X_train, X_test, y_train, y_test):
+def train_model(reg_rate, X_train, y_train):
     # train model
-    LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
+    model = LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
+    return model
 
 
 def parse_args():
@@ -59,16 +65,9 @@ def parse_args():
 
 # run script
 if __name__ == "__main__":
-    # add space in logs
-    print("\n\n")
-    print("*" * 60)
-
     # parse args
     args = parse_args()
 
     # run main function
     main(args)
 
-    # add space in logs
-    print("*" * 60)
-    print("\n\n")
